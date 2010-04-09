48cd581b79199076d9561e5e998f6a3057eb418b
Derek Arnold <github@vyr.us>
2010-04-09 13:53:42 -0500

    Renaming the module to better reflect its new scope

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
-setup(name='pgoptparse',
+setup(name='pglunchbox',
       version='1.1.0',
       description='A convenience module that extends optparse for scripting against a PostgreSQL database.',
       author='Derek Arnold',
       author_email='derek@dderek.com',
       license='MIT License',
-      url='http://github.com/lysol/pgoptparse',
-      py_modules=['pgoptparse'],
+      url='http://github.com/lysol/pglunchbox',
+      py_modules=['pglunchbox'],
      )
